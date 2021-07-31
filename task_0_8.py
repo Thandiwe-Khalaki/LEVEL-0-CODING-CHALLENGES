f641ca3e268a0ba0b6ff850270a7c93c4b43cad3
Thandiwe Khalaki <66163901+Thandiwe-Khalaki@users.noreply.github.com>
2021-07-31 17:02:53 +0200

    fixed task 0.8

--- task_0_8.py
+++ task_0_8.py
@@ -7,10 +7,12 @@ def number_to_time(number):
         print(f"{hours} hours, {minutes} minutes")
     elif hours < 2 and minutes > 1:
         print(f"{hours} hour, {minutes} minutes")
+    elif hours == 0 and minutes == 0:
+        print(f"{hours} hours, {minutes} minutes")
     elif hours > 1 and minutes < 2:
         print(f"{hours} hours, {minutes} minute")
     else:
         print(f"{hours} hour, {minutes} minute")
 
 
-number_to_time(133)
+number_to_time(0)

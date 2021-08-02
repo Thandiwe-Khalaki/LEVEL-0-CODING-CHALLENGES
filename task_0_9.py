f82ab96c00c9d20a02be8a42ba416269fa11b64c
Thandiwe Khalaki <66163901+Thandiwe-Khalaki@users.noreply.github.com>
2021-08-03 00:38:12 +0200

    Revert "combined line four and five"
    This reverts commit c18735d45388ea533f96b10ce08f1a396bc35ff5.

--- task_0_9.py
+++ task_0_9.py
@@ -1,7 +1,8 @@
 def string_to_vowel(word):
     vowels = []
     for letter in word.lower():
-        if letter in ("aeiou") and letter not in vowels:
+        if letter in ("aeiou"):
+            if letter not in vowels:
                 vowels.append(letter)
 
     print("Vowels: " + ", ".join(vowels))

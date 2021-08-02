f9cfa8040d5f396ed7f4afb93e89f9096218012f
Thandiwe Khalaki <66163901+Thandiwe-Khalaki@users.noreply.github.com>
2021-08-02 12:05:57 +0200

    fixed duplicate letters

--- task_1_0.py
+++ task_1_0.py
@@ -2,14 +2,16 @@ def common_letters(first_word, second_word):
     first_word = first_word.casefold()
     second_word = second_word.casefold()
     common = ""
-    for letter in first_word:
-        if letter in second_word:
+    for letter in set(first_word):
+        if letter in set(second_word):
             common += letter
+
     print("Common letters: " + ", ".join(common))
 
 
 def main():
-    common_letters("Eckard", "Berry")
+    common_letters("EckardR", "Berry")
 
 if __name__ == "__main__":
     main()
+

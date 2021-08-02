fc78559b4a62f0a0df41cba1dd468952d3b047ff
Thandiwe Khalaki <66163901+Thandiwe-Khalaki@users.noreply.github.com>
2021-08-02 20:56:06 +0200

    removed set() and fixed duplicate letters

--- task_1_0.py
+++ task_1_0.py
@@ -2,9 +2,9 @@ def common_letters(first_word, second_word):
     first_word = first_word.casefold()
     second_word = second_word.casefold()
     common = ""
-    for letter in set(first_word):
-        if letter in set(second_word):
-            common += letter
+    for letter in range(len(first_word)):
+        if (first_word[letter] in second_word) and (first_word[letter] not in common):
+            common += first_word[letter]
 
     print("Common letters: " + ", ".join(common))
 

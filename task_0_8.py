526b077ab8dbb959f373c3ddc1ea4395dae80fc1
Thandiwe Khalaki <66163901+Thandiwe-Khalaki@users.noreply.github.com>
2021-08-04 00:37:43 +0200

    fixed the plurals

--- task_0_8.py
+++ task_0_8.py
@@ -3,12 +3,12 @@ def number_to_time(number):
     hour = int(number // 60)
 
     minutes = "minute" if (minute == 1) else "minutes"
-    hours = "hours" if (hour == 1) else "hours"
+    hours = "hour" if (hour == 1) else "hours"
     print(f"{int(number//60)} {hours}, {int(number%60)} {minutes}")
 
 
 def main():
-    number_to_time(360)
+    number_to_time(1)
 
 if __name__ == "__main__":
     main()
